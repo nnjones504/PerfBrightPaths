39ad4937ba734c4ec216118577fa695cbe255beb
Natalie Jones <nnjones99@gmail.com>
2021-10-26 22:12:20 -0500

    solved challenge

--- Challenges/10-26-21-challenge-stock-prices/stockPrices.js
+++ Challenges/10-26-21-challenge-stock-prices/stockPrices.js
@@ -1,19 +1,16 @@
-let arr = [15, 10, 20, 22, 1, 9]
-let max = arr[0]
-let min = arr[0]
-let profit = 0;
+/** @format */
 
-let best = arr => {
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] > max){
-      max = arr[i]
-    }
-    if (arr[i] < min && arr.indexOf(arr[i]) < arr.indexOf(max)){
-      min = arr[i]
-    }
-  }
-  profit = max - min
-  return `$${profit} Buy at $${min} and sell at $${max}`
-}
+const best = (arr) => {
+	let profit = 0;
+	let min = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		} else if (arr[i] - min > profit) {
+			profit = arr[i] - min;
+		}
+	}
+	return profit;
+};
 
-console.log(best(arr));
+console.log(best([15, 10, 20, 22, 1, 9]));
