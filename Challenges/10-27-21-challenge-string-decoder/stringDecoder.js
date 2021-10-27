42519e71f092696ce4f9eadaeddd6437d2036290
Natalie Jones <nnjones99@gmail.com>
2021-10-27 10:04:56 -0500

    made more concise

--- Challenges/10-27-21-challenge-string-decoder/stringDecoder.js
+++ Challenges/10-27-21-challenge-string-decoder/stringDecoder.js
@@ -1,24 +1,18 @@
 /** @format */
 
-
 const decoder = (code) => {
 	let i = 0;
 	code = code.split("");
 	let str = [];
 	while (i < code.length) {
 		if (code[i].match(/^[0-9]*$/)) {
-			i += Number(code[i]) + 1;
-      {
-        if (i == 0){
-          i += 1;
-        }
-      }
+			i += Number(code[i]);
 		} else {
 			str.push(code[i]);
-			i += 1;
 		}
+		i+=1;
 	}
-  return str.join("");
+  return str.join("")
 };
 
 console.log(decoder("0y4akjfes"));
