998cd3fda6ce6f48522458d4a2b4b7b7cd7a811d
Natalie Jones <nnjones99@gmail.com>
2021-10-29 12:00:55 -0500

    finished challenge

--- Challenges/10-29-21-chal-missing-number/missingNumber.js
+++ Challenges/10-29-21-chal-missing-number/missingNumber.js
@@ -1,9 +1,11 @@
+/** @format */
+
 const missingNumber = (arr, max) => {
-  for (let i = 1; i <= max; i++){
-    if (arr.includes(i) === false){
-      return i;
-    }
-  }
-}
+	for (let i = 1; i <= max; i++) {
+		if (arr.includes(i) === false) {
+			return i;
+		}
+	}
+};
 
 console.log(missingNumber([1, 2, 3, 4, 5, 6, 7, 9, 10], 10));
