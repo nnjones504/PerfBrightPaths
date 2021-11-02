53e6b398434de6043d4e9e74ef6db42828afbcee
Natalie Jones <nnjones99@gmail.com>
2021-11-02 09:23:32 -0500

    solved using loop and conditional

--- Challenges/11-02-21-chal-make-unique-string/makeUnique.js
+++ Challenges/11-02-21-chal-make-unique-string/makeUnique.js
@@ -5,3 +5,19 @@ const makeUnique = str => {
 }
 
 console.log(makeUnique("helloworld"));
+
+// the instructions said to use a loop and conditional so here is the loop and conditional method
+const makeUnique2 = (str) => {
+	str = str.split("");
+	let newStr = [];
+	let i = 0;
+	while (i < str.length) {
+		if (!newStr.includes(str[i])) {
+			newStr.push(str[i]);
+		}
+    i++;
+	}
+	return newStr.join("");
+};
+
+console.log(makeUnique2("iwanttoclimbamountain"));
