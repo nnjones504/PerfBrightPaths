6d398df1d0964ec3ec9d6259124e084dc826fc0c
Natalie Jones <nnjones99@gmail.com>
2021-11-23 08:02:39 -0600

    server finished being set up

--- Assessments/assessment-qa-devops-11-19-21/server.js
+++ Assessments/assessment-qa-devops-11-19-21/server.js
@@ -6,6 +6,15 @@ const {shuffleArray} = require('./utils')
 
 app.use(express.json())
 
+app.use(express.static("public"));
+
+app.get("/styles", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.css"));
+});
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.js"));
+});
+
 app.get('/api/robots', (req, res) => {
     try {
         res.status(200).send(botsArr)
