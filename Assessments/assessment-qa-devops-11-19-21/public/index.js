ba3b0b177a15553c866b53b5cfbcb3319ad8220f
Natalie Jones <nnjones99@gmail.com>
2021-11-23 08:04:59 -0600

    fixed to work with localhost

--- Assessments/assessment-qa-devops-11-19-21/public/index.js
+++ Assessments/assessment-qa-devops-11-19-21/public/index.js
@@ -118,7 +118,7 @@ const putBotBack = (id) => {
 }
 
 const drawFive = () => {
-    axios.get('/api/robots/five')
+    axios.get('http://localhost:3000/api/robots/five')
         .then(res => {
             choices = res.data.choices
             compDuo = res.data.compDuo
@@ -135,7 +135,7 @@ const duel = () => {
     renderCompDuo()
     document.querySelectorAll('.bot-btn').forEach(btn => btn.classList.add('hide'))
     setTimeout(() => {
-        axios.post('/api/duel', {compDuo, playerDuo})
+        axios.post('http://localhost:3000/api/duel', {compDuo, playerDuo})
             .then(({data}) => {
                 resultsText.textContent = data
                 playAgainBtn.classList.remove('hide')
@@ -158,7 +158,7 @@ const reset = () => {
 }
 
 const getPlayerStats = () => {
-    axios.get('/api/player')
+    axios.get('http://localhost:3000/api/player')
         .then(({data: {wins, losses}}) => {
             winsText.textContent = `Wins: ${wins}`
             lossesTest.textContent = `Losses: ${losses}`
@@ -166,7 +166,7 @@ const getPlayerStats = () => {
 }
 
 const getAllBots = () => {
-    axios.get('/api/robots')
+    axios.get('http://localhost:3000/api/robots')
         .then(({data}) => {
             allBotsDiv.innerHTML = ''
         
